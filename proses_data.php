2a9a904d0ffef085a1e0ec30219e7814f1071c58
Aristyo Hadikusuma <tyokusuma@Aristyos-MacBook-Pro.local>
2020-01-22 14:27:34 +0700

    upload semua fitur

---
+++ proses_data.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    if (i == pria) {
+        <input type="radio" value="">pria
+    }else
+    <input type="radio" value="">wanita
+    ?>
+</body>
+</html>
